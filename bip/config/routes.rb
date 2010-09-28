282c920a5b095eeadf80dcffac6b6d127354889d
MPV <victor.sollerhed@gmail.com>
2010-09-28 22:20:22 +0200

    Started styling Rails app like the html prototype.

--- bip/config/routes.rb
+++ bip/config/routes.rb
@@ -54,7 +54,7 @@ Bip::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "playlists#index"
 
   # See how all your routes lay out with "rake routes"
 
