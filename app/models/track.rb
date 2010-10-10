df3da5f813d8601ff7c2db9acb093556a6f2dd6a
MPV <victor.sollerhed@gmail.com>
2010-10-10 16:30:54 +0200

    Now supports tracks with weird (e.g. Swedish) characters when getting cover image. Using CGI::escape().

--- app/models/track.rb
+++ app/models/track.rb
@@ -11,8 +11,9 @@ class Track < ActiveRecord::Base
   def album_image_url
     require 'net/http'
     require 'json'
+    #require 'CGI'
     bip_last_fm_api_key = "df16509ddceedad8dc111fa81a6154f6"
-    url = URI.parse("http://ws.audioscrobbler.com/2.0/?method=track.getinfo&api_key=#{bip_last_fm_api_key}&format=json&artist=#{artist.gsub("\ ", "+")}&track=#{name.gsub("\ ", "+")}")
+    url = URI.parse("http://ws.audioscrobbler.com/2.0/?method=track.getinfo&api_key=#{bip_last_fm_api_key}&format=json&artist=#{CGI::escape(artist)}&track=#{CGI::escape(name)}")
     resp = Net::HTTP.get_response(url)
     data = resp.body
     result = JSON.parse(data)
