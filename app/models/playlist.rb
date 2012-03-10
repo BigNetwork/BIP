bd71eea99de08630b86a13b11f8132247d892edf
Victor Sollerhed <victor@sollerhed.se>
2012-03-10 22:23:06 +0100

    Cleanup.

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,12 +1,11 @@
 class Playlist < ActiveRecord::Base
   #has_many :tracks, :order => 'votes_count DESC'
   has_many :playlist_items, :order => 'votes_count DESC, created_at ASC'
-  #has_many :tracks, :through => :playlist_items
+  has_many :tracks, :through => :playlist_items
   has_many :plays, :through => :playlist_items, :order => :created_at
   has_many :votes, :through => :playlist_items
   
   def latest_status_at
-    logger.info "playlist #{id} has #{plays.count} plays"
     if is_playing? && plays.first
       plays.first.created_at
     else
