24dd64ae60d623645acae9b8c9276c01e4cf4099
Victor Sollerhed <victor@sollerhed.se>
2012-03-10 19:15:46 +0100

    Använder jquery.timeago.js för att med JS ändra '5 sekunder sedan'-tidsstämplar för spellistor.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,6 +2,31 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 $(function(){
+	
+	// Svensk översättning av timeago:
+	$.timeago.settings.strings = {
+	  prefixAgo: "för",
+	  prefixFromNow: "om",
+	  suffixAgo: "sedan",
+	  suffixFromNow: "",
+	  seconds: "mindre än en minut",
+	  minute: "ungefär en minut",
+	  minutes: "%d minuter",
+	  hour: "ungefär en timme",
+	  hours: "ungefär %d timmar",
+	  day: "en dag",
+	  days: "%d dagar",
+	  month: "ungefär en månad",
+	  months: "%d månader",
+	  year: "ungefär ett år",
+	  years: "%d år",
+		wordSeparator: " ",
+		numbers: []
+	};
+	
+	// Kontinuerlig javascript-uppdatering av datum:
+	$("abbr.timeago").timeago();
+	
 	$("table#all_tracks").dataTable({
 		//"aaSorting": [[],[ 1, "desc" ], [ 0, "asc" ]],
 		//"aoColumns": [ 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def timeago(time, options = {})
+    options[:class] ||= "timeago"
+    if options[:inner_html]
+      inner_html = options[:inner_html]
+      options[:inner_html] = nil
+    else
+      inner_html = time.to_s
+    end
+    content_tag(:abbr, inner_html, options.merge(:title => time.getutc.iso8601)) if time
+  end
+
 end
